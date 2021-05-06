4a6bbe05f9eb2e9251261c5498ef286db778722a
Nick Genovese <ngenovese@pepperdash.com>
2021-05-06 10:19:25 -0400

    initial dev

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("DmMd6xnEpi")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DmMd6xnEpi")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
